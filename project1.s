351cc500bd62311c8c2a96e69a67989b28a22f41
tysongouche <39385160+tysongouche@users.noreply.github.com>
2021-10-30 17:39:08 -0400

    Added Changes
    -Completed the reading input call
    -Started the beginning of my byte addressing for input

--- project1.s
+++ project1.s
@@ -6,3 +6,7 @@
 main: 								#Main Section Starts
 	li $v0, 8						#Load Read input command
 	la $a0, input						#Mem Address from input is now destination MA
+	li $a1, 11						#Setting length to be 11 bytes
+	syscall
+	
+	li $t0, 0						#This register will be used for byte addressing
